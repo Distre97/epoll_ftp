15328590635c79e07712c5174650f64981a17832
distre97 <yuzhenqi97@foxmail.com>
2020-05-09 18:44:09 +0800

    write .h files

---
+++ ftpServer.h
@@ -0,0 +1,24 @@
+#ifndef _FTPSERVER_H
+#define _FTPSERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <errno.h>
+
+#include 'GLOBAL.h'
+
+
+
+
+
+
+
+
+
+
+#endif
